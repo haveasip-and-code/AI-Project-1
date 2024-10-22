9fa64fda2d6710738499364378c979a779a07a46
KhoiLeeee <22125042@student.hcmus.edu.vn>
2024-10-22 21:15:59 +0700

    fix weight number on stone
    fix the position of weight on stone in center

--- src/window.py
+++ src/window.py
@@ -92,13 +92,33 @@ class Window:
                     self.canvas.create_oval(x1, y1, x2-10, y2-10, fill="blue", outline="blue")
                     for x, y, weight in stones:
                         if (i, j) == (x, y):
-                            self.canvas.create_text(x1+15, y1+15, text=str(weight), fill="white")
+                            center_x = (x1 + x2 - 10) / 2
+                            center_y = (y1 + y2 - 10) / 2
+
+                            stone_width = x2 - x1 - 10
+                            stone_height = y2 - y1 - 10
+
+                            # Set the font size as a fraction of the stone's height (adjust as needed)
+                            font_size = int(stone_height * 0.25)
+
+                            # Create the text with the calculated font size and center position
+                            self.canvas.create_text(center_x, center_y, text=str(weight), fill="white", font=("Arial", font_size))
                             break
                 elif cell == '*':
                     self.canvas.create_rectangle(x1, y1, x2, y2, fill="pink", outline="pink")
                     for x, y, weight in stones:
                         if (i, j) == (x, y):
-                            self.canvas.create_text(x1+10, y1+10, text=str(weight), fill="black")
+                            center_x = (x1 + x2) / 2
+                            center_y = (y1 + y2) / 2
+                            
+                            switch_width = x2 - x1
+                            switch_height = y2 - y1
+
+                            # Set the font size as a fraction of the stone's height (adjust as needed)
+                            font_size = int(switch_height * 0.25)
+
+                            # Create the text with the calculated font size and center position
+                            self.canvas.create_text(center_x, center_y, text=str(weight), fill="black", font=("Arial", font_size))
                             break
 
         self.master.update_idletasks()
